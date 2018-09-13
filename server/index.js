7ff9dc4248c0f4a27e6959ec79d286a9af805f3b
Loiane <loianeg@gmail.com>
2018-09-13 16:47:07 -0400

    nodemon

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+// A default hello word route
+app.get('/', (req, res) => {
+    res.send({hello: 'world'});
+});
+
 const port = process.env.port || '3000';
 
 app.listen(port, () => console.log(`API running on port ${port}`));
