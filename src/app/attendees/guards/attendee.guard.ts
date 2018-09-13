289d1eb5cbe749c1b79839ea6191af6df15d0d93
Loiane <loianeg@gmail.com>
2018-09-13 17:44:57 -0400

    added guard resolver

--- src/app/attendees/guards/attendee.guard.ts
+++ src/app/attendees/guards/attendee.guard.ts
@@ -1,17 +1,29 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 import { Attendee } from '../model/attendee';
+import { AttendeesService } from '../services/attendees.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AttendeeGuard implements Resolve<Attendee> {
+
+  constructor(private service: AttendeesService) {}
+
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Attendee | Observable<Attendee> | Promise<Attendee> {
-    throw new Error('Method not implemented.');
+
+    if (route.params != null && route.params.id != null) {
+      return this.service.loadById(route.params.id);
+    }
+    return of({
+      _id: null,
+      name: null,
+      email: null
+    });
   }
 }
