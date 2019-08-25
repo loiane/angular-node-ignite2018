94a5674507dc74ca57d3c028725d336bb5797dab
Loiane <loianeg@gmail.com>
2019-08-25 10:40:39 -0400

    updated connection string

--- server/src/mongo/mongo.js
+++ server/src/mongo/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/ignite2018';
+const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/crud-mean';
 
 function connect() {
  mongoose.set('debug', true);
